80df3aef92f3f1f421cb3075a1c14455fb5367f6
aschams <acschams@gmail.com>
2019-09-05 10:33:08 -0400

    reset to working app

--- marsican.py
+++ marsican.py
@@ -87,7 +87,8 @@ def upload_file():
     <h1>Upload new File</h1>
     <form method=post enctype=multipart/form-data>
       <input type=file name=file>
-      <input type=submit value=Uploadz>
+      <input type="range" min="0" max="100" name="threshold_slider"/>
+      <input type=submit value=Upload>
     </form>
     '''
 
